1df180bdc151a05fc90179bd4a6354b9b99cb4af
morpheusthewhite <zffromGerace@hotmail.it>
2019-05-15 19:37:28 +0200

    Add count of detected faces

--- celephais/core.py
+++ celephais/core.py
@@ -8,11 +8,17 @@ def main():
     parser = utils.CelephaisParser()
     parsed_args = parser.parse_args()
 
+    if parsed_args.image is None:
+        parser.print_help()
+        return
+
     img_path = os.path.join(os.getcwd(), parsed_args.image)
 
     img = cv.imread(img_path)
 
     face_detection.face_detection_draw_rectangles(img)
+    print("detected " + str(face_detection.face_detection_count(img)) +" faces")
+
     cv.imshow('sample image', img)
     cv.waitKey(0)
     cv.destroyAllWindows()
--- celephais/face_detection.py
+++ celephais/face_detection.py
@@ -11,7 +11,7 @@ CLASSIFIER_FILENAME_PROFILE = pkg_resources.resource_filename(__name__,
                                                             os.path.join("..",
                                                                          CLASSIFIERS_FOLDER +
                                                                          '/haarcascade_profileface.xml'))
-OVERLAPPING_THRESHOLD=0.9
+OVERLAPPING_THRESHOLD = 0.9
 
 face_cascade_frontal = cv.CascadeClassifier(os.path.join(CLASSIFIERS_FOLDER, CLASSIFIER_FILENAME_FRONTAL))
 face_cascade_profile = cv.CascadeClassifier(os.path.join(CLASSIFIERS_FOLDER, CLASSIFIER_FILENAME_PROFILE))
@@ -21,30 +21,10 @@ def face_detection_draw_rectangles(img_raw):
     # convert image to gray scale in order to detect faces
     gray = cv.cvtColor(img_raw, cv.COLOR_BGR2GRAY)
 
-    # detect faces
-    faces_profile = face_cascade_profile.detectMultiScale(gray, 1.3, 5)
-    faces_frontal = face_cascade_frontal.detectMultiScale(gray, 1.3, 5)
-
-    # merge faces detected
-    faces = list(faces_profile) + list(faces_frontal)
-
-    valid_faces = []
-    # filter overlapping faces
-    for i in range(len(faces)):
-        (x1, y1, w1, h1) = faces[i]
-        other_faces = faces[i+1:]
-
-        valid_faces.append(faces[i])
-
-        for (x2, y2, w2, h2) in other_faces:
-            overlapping_area = get_overlapping_area(x1, y1, w1, h1, x2, y2, w2, h2)
-
-            if(overlapping_area / w1 * h1 > OVERLAPPING_THRESHOLD):
-                valid_faces.pop()
-                break
+    faces = face_detection_get_faces(gray)
 
     # draw non overlapping faces
-    for (x, y, w, h) in valid_faces:
+    for (x, y, w, h) in faces:
         cv.rectangle(img_raw, (x, y), (x + w, y + h), (255, 0, 255), 2)
 
 
@@ -63,3 +43,34 @@ def get_overlapping_area(x1, y1, w1, h1, x2, y2, w2, h2):
     overlapping_width = min(y1 + h1, y2 + h2) - max(y1, y2)
 
     return overlapping_height * overlapping_width
+
+def face_detection_get_faces(img_gray):
+    # detect faces
+    faces_profile = face_cascade_profile.detectMultiScale(img_gray, 1.3, 5)
+    faces_frontal = face_cascade_frontal.detectMultiScale(img_gray, 1.3, 5)
+
+    # merge faces detected
+    faces = list(faces_profile) + list(faces_frontal)
+
+    valid_faces = []
+    # filter overlapping faces
+    for i in range(len(faces)):
+        (x1, y1, w1, h1) = faces[i]
+        other_faces = faces[i + 1:]
+
+        valid_faces.append(faces[i])
+
+        for (x2, y2, w2, h2) in other_faces:
+            overlapping_area = get_overlapping_area(x1, y1, w1, h1, x2, y2, w2, h2)
+
+            if (overlapping_area / w1 * h1 > OVERLAPPING_THRESHOLD):
+                valid_faces.pop()
+                break
+
+    return valid_faces
+
+def face_detection_count(img_raw):
+    # convert image to gray scale in order to detect faces
+    gray = cv.cvtColor(img_raw, cv.COLOR_BGR2GRAY)
+
+    return len(face_detection_get_faces(gray))
