449b53c14fdda4505ffc5cd646636af5d6aa7861
morpheusthewhite <zffromGerace@hotmail.it>
2019-05-16 22:28:20 +0200

    Add support for detecting image and xml folders

--- celephais/core.py
+++ celephais/core.py
@@ -5,6 +5,7 @@ import json
 
 from celephais import face_detection
 from celephais import metadata
+from celephais import data_parse
 
 # creating top-level parser
 parser = argparse.ArgumentParser(prog="Celephais")
@@ -15,43 +16,61 @@ parser_detection = subparsers.add_parser('detect')
 source_group = parser_detection.add_mutually_exclusive_group(required=True)
 source_group.add_argument("--image", type=str, help="the image file in which to detect faces")
 source_group.add_argument("--xml", type=str, help="the xml file which contains required metadata")
-parser_detection.add_argument("--show", action="store_true", help="show the detected faces in a window, "
+out_group = parser_detection.add_mutually_exclusive_group(required=False)
+out_group.add_argument("--show", action="store_true", help="show the detected faces in a window, "
                                                                   "otherwise just prints the number")
-parser_detection.add_argument("--ojson", help="if specified, and if --xml is given, metadata are saved "
+out_group.add_argument("--ojson", help="if specified, and if --xml is given, metadata are saved "
                                               "with the given filename")
+
 
 def main():
     parsed_args = parser.parse_args()
 
+    img_paths = []
     if parsed_args.image is not None:
-        img_path = os.path.join(os.getcwd(), parsed_args.image)
-    else: # xml is given instead
-        xml_path = os.path.join(os.getcwd(), parsed_args.xml)
-        parsed_dict = metadata.xml_parse(xml_path)
+        img_paths = data_parse.parse_images(parsed_args.image)
+    else:  # xml is given instead
+        xml_paths = data_parse.parse_xmls(parsed_args.xml)
 
-        # calculate path to image from xml file
-        img_filename = parsed_dict["photo"]
-        img_path = os.path.join(os.path.split(xml_path)[0], img_filename)
+        # for each xml the path to the associated image is calculated
+        dicts_parsed = []
+        for xml_path in xml_paths:
+            parsed_dict = metadata.xml_parse(xml_path)
+            dicts_parsed.append(parsed_dict)
 
-    img = cv.imread(img_path)
+            img_filename = parsed_dict["photo"]
+            img_paths.append(os.path.join(os.path.split(xml_path)[0], img_filename))
 
-    if parsed_args.show:
-        face_detection.face_detection_draw_rectangles(img)
-        cv.imshow('sample image', img)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
-    else: # if show is not specified the number of face detected are printed on stdout
-        face_detected = face_detection.face_detection_count(img)
-        print("detected " + str(face_detected) +" faces in " + img_path)
+    # TODO: add list for dicts and write them to fle
+    dicts_detected = []
 
-    # save the final json file
-    if parsed_args.xml is not None and parsed_args.ojson is not None:
-        parsed_dict.pop("photo")
+    for img_path in img_paths:
+        img = cv.imread(img_path)
 
-        parsed_dict["students"] = face_detected
+        if parsed_args.show:
+            face_detection.face_detection_draw_rectangles(img)
+            cv.imshow('sample image', img)
+            cv.waitKey(0)
+            continue
+        else:  # if show is not specified the number of face detected are printed on stdout
+            face_detected = face_detection.face_detection_count(img)
+            print("detected " + str(face_detected) +" faces in " + img_path)
 
+        # save the final json file
+        if parsed_args.xml is not None and parsed_args.ojson is not None:
+            parsed_dict = dicts_parsed.pop(0)
+            parsed_dict.pop("photo")
+
+            parsed_dict["students"] = face_detected
+
+            dicts_detected.append(parsed_dict)
+
+    if parsed_args.ojson is not None:
         with open(os.path.join(os.getcwd(), parsed_args.ojson), "w") as f:
-            f.write(json.dumps(parsed_dict))
+            f.write(json.dumps(dicts_detected))
+
+    cv.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main()
---
+++ celephais/data_parse.py
@@ -0,0 +1,41 @@
+import os
+
+
+def parse_images(image_path):
+    """
+    returns a list of the absolute paths to the images (more than 1 if image_path is a folder, 1 if is a file)
+    """
+    images = []
+
+    # if a directory is passed
+    if os.path.isdir(image_path):
+        images_listed = os.listdir(image_path)
+
+        # then all the files in the dir will be analyzed
+        for img in images_listed:
+            images.append(os.path.join(os.getcwd(), image_path, img))
+    else:
+        # only the given image is analyzed
+        images.append(os.path.join(os.getcwd(), image_path))
+
+    return images
+
+
+def parse_xmls(xml_path):
+    """
+    returns a list of the absolute paths to the xmls (more than 1 if xml_path is a folder, 1 if is a file)
+    """
+    xml_paths = []
+
+    # if a directory is passed
+    if os.path.isdir(xml_path):
+        xmls_listed = os.listdir(xml_path)
+
+        # then all the files in the dir will be analyzed
+        for xml in xmls_listed:
+            xml_paths.append(os.path.join(os.getcwd(), xml_path, xml))
+    else:
+        # only the given xml is analyzed
+        xml_paths.append(os.path.join(os.getcwd(), xml_path))
+
+    return xml_paths
