3eb2f0996cef357fed8cdf9bba7054282728781e
morpheusthewhite <zffromGerace@hotmail.it>
2019-05-16 08:51:52 +0200

    Improve cl argument parser

--- celephais/core.py
+++ celephais/core.py
@@ -1,27 +1,34 @@
 import cv2 as cv
 import os
+import argparse
 
-from celephais import utils
 from celephais import face_detection
 
-def main():
-    parser = utils.CelephaisParser()
-    parsed_args = parser.parse_args()
+# creating top-level parser
+parser = argparse.ArgumentParser(prog="Celephais")
+subparsers = parser.add_subparsers(help="sub-command help")
 
-    if parsed_args.image is None:
-        parser.print_help()
-        return
+# creating parser for the detection command
+parser_detection = subparsers.add_parser('detect')
+parser_detection.add_argument("--image", required=True, type=str, help="the image file in which to detect faces")
+parser_detection.add_argument("--show", action="store_true", help="show the detected faces in a window, "
+                                                                  "otherwise just prints the number")
+
+def main():
+    parsed_args = parser.parse_args()
 
     img_path = os.path.join(os.getcwd(), parsed_args.image)
 
     img = cv.imread(img_path)
 
-    face_detection.face_detection_draw_rectangles(img)
-    print("detected " + str(face_detection.face_detection_count(img)) +" faces")
+    if parsed_args.show:
+        face_detection.face_detection_draw_rectangles(img)
+        cv.imshow('sample image', img)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+    else:
+        print("detected " + str(face_detection.face_detection_count(img)) +" faces")
 
-    cv.imshow('sample image', img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
--- celephais/utils.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-class CelephaisParser(argparse.ArgumentParser):
-    def __init__(self):
-        super(CelephaisParser, self).__init__()
-
-        self.add_argument("--image", help="the image in which to detect faces", type=str)
