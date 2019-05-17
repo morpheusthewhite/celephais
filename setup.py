c9443be15264674672b3fde9fa9e13188632da39
morpheusthewhite <zffromGerace@hotmail.it>
2019-05-17 19:10:31 +0200

    Update required packages

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    "opencv-python", "keras", "pandas"
+    "opencv-python", "keras", "pandas", "scikit-learn"
 ]
 
 # What packages are optional?
