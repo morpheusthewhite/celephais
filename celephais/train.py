25ba8f300265276ac4400a0f2e4333f38f1f6720
morpheusthewhite <zffromGerace@hotmail.it>
2019-05-18 08:28:59 +0200

    Implement data transformation function

--- celephais/train.py
+++ celephais/train.py
@@ -30,30 +30,42 @@ class StudentsEstimator:
         self.Y_train = df["students"]
         X_unencoded = df.drop("students", axis=1)
 
-        # encode subjects
-        subjects_unencoded = X_unencoded["subject"]
-
         # creating encoders for next encodings
         self.label_encoder_subjects = LabelEncoder()
         self.one_hot_encoder_subjects = OneHotEncoder(sparse=False)
-        int_label_subjects = self.label_encoder_subjects.fit_transform(subjects_unencoded).reshape(-1, 1)
-        subjects_frame = pandas.DataFrame(self.one_hot_encoder_subjects.fit_transform(int_label_subjects))
-
-        X_subjects_encoded = pandas.concat([subjects_frame, X_unencoded.drop("subject", axis=1)], axis=1)
-
-        # encode days
-        days_unencoded = df["day"]
-
-        # creating encoders for next encodings
         self.label_encoder_days = LabelEncoder()
         self.one_hot_encoder_days = OneHotEncoder(sparse=False)
-        int_label_days = self.label_encoder_days.fit_transform(days_unencoded).reshape(-1, 1)
-        days_frame = pandas.DataFrame(self.one_hot_encoder_days.fit_transform(int_label_days))
 
-        self.X_train = pandas.concat([days_frame, X_subjects_encoded.drop("day", axis=1)], axis=1)
+        self.X_train = self.transform_data(X_unencoded, fit=True)
 
         self.estimator = KerasRegressor(build_fn=baseline_model, inputs=len(self.X_train.columns), epochs=100,
                          batch_size=5, verbose=0)
+
+    def transform_data(self, data_frame, fit=False):
+
+        # encode subjects
+        subjects_unencoded = data_frame["subject"]
+
+        if fit:
+            int_label_subjects = self.label_encoder_subjects.fit_transform(subjects_unencoded).reshape(-1, 1)
+            subjects_frame = pandas.DataFrame(self.one_hot_encoder_subjects.fit_transform(int_label_subjects))
+        else:
+            int_label_subjects = self.label_encoder_subjects.transform(data_frame).reshape(-1, 1)
+            subjects_frame = pandas.DataFrame(self.one_hot_encoder_subjects.transform(int_label_subjects))
+
+        X_subjects_encoded = pandas.concat([subjects_frame, data_frame.drop("subject", axis=1)], axis=1)
+
+        # encode days
+        days_unencoded = data_frame["day"]
+
+        if fit:
+            int_label_days = self.label_encoder_days.fit_transform(days_unencoded).reshape(-1, 1)
+            days_frame = pandas.DataFrame(self.one_hot_encoder_days.fit_transform(int_label_days))
+        else:
+            int_label_days = self.label_encoder_days.transform(days_unencoded).reshape(-1, 1)
+            days_frame = pandas.DataFrame(self.one_hot_encoder_days.transform(int_label_days))
+
+        return pandas.concat([days_frame, X_subjects_encoded.drop("day", axis=1)], axis=1)
 
     def train(self):
         # training model
@@ -62,20 +74,5 @@ class StudentsEstimator:
     def predict(self, prediction_data):
         dp = pandas.DataFrame(prediction_data)
 
-        # encode subjects
-        subjects_unencoded = dp["subject"]
-
-        int_label_subjects = self.label_encoder_subjects.transform(subjects_unencoded).reshape(-1, 1)
-        subjects_frame = pandas.DataFrame(self.one_hot_encoder_subjects.transform(int_label_subjects))
-
-        X_subjects_encoded = pandas.concat([subjects_frame, dp.drop("subject", axis=1)], axis=1)
-
-        # encode days
-        days_unencoded = dp["day"]
-
-        int_label_days = self.label_encoder_days.transform(days_unencoded).reshape(-1, 1)
-        days_frame = pandas.DataFrame(self.one_hot_encoder_days.transform(int_label_days))
-
-        X = pandas.concat([days_frame, X_subjects_encoded.drop("day", axis=1)], axis=1)
-
+        X = self.transform_data(dp)
         return self.estimator.predict(X)
